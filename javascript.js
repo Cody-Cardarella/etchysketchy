975e4161be555de54ca4aeb5a6405a1607459f04
Cody <115433936+Cody-Cardarella@users.noreply.github.com>
2022-11-11 08:11:17 -0700

    Add RBG to the etchysketchy

--- javascript.js
+++ javascript.js
@@ -8,9 +8,10 @@ container.classList.add('container');
 let clearBtn = document.createElement('button')
 body.appendChild(clearBtn);
 clearBtn.textContent = 'Clear';
+clearBtn.style.cssText = "margin: 50px;"
 
 // Style the body
-body.style.cssText = 'display:flex; align-items: center;justify-content: center; margin-top: 50px;'
+body.style.cssText = 'display:flex; align-items: center;justify-content: center;flex-direction: column; margin-top: 50px;'
 
 //Style the first container
 container.style.cssText = 'border: 1px solid black; display: inline-grid;grid-template-columns: repeat(8, 1fr);grid-template-rows: repeat(8, 1fr);height: 400px; width: 400px;'
@@ -20,6 +21,7 @@ for (i = 1; i <= 64; i ++){
     let subDiv = document.createElement('div');
     container.appendChild(subDiv);
     subDiv.classList.add('colorThisDiv')
+    // subDiv.style.cssText = 'border: 1px solid black;'
 }
 
 // Grab all divs
@@ -28,7 +30,7 @@ let colorDivs = document.querySelectorAll('.colorThisDiv');
 // Change color of divs on mouseover
 colorDivs.forEach((divs) => {
     divs.addEventListener('mouseover', function(event){
-        event.target.style.background = 'black';
+        event.target.style.background = rainbowColor();
     })
 
 });
@@ -39,3 +41,14 @@ colorDivs.forEach((divs) => {
         divs.style.background = null;
     })
 });
+
+
+function rainbowColor(){
+    let x = Math.floor(Math.random()*265)
+    let y = Math.floor(Math.random()*256)
+    let z = Math.floor(Math.random()*256)
+    let color = "rgb(" + x + "," + y + "," + z + ")"; 
+    return color;
+}
+
+console.log(rainbowColor());
